300c5817fb54f79129e2598e33d23d12258b5bfa
Liu Xing <xing@57blocks.com>
2026-06-01 14:09:00 +0800

    feat: add selectFolder to window.electronAPI TypeScript declarations

--- src/types/electron.d.ts
+++ src/types/electron.d.ts
@@ -1,6 +1,7 @@
 export interface ElectronAPI {
   getPlatform: () => Promise<string>;
   getAppVersion: () => Promise<string>;
+  selectFolder: () => Promise<string | null>;
   isElectron: boolean;
 }
 
