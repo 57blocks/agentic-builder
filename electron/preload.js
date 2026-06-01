c9c7d9acad937a381bc05a30570048f9c8d36f1d
Liu Xing <xing@57blocks.com>
2026-06-01 14:06:47 +0800

    feat: add select-folder IPC handler via Electron dialog

--- electron/preload.js
+++ electron/preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
   getPlatform: () => ipcRenderer.invoke("get-platform"),
   getAppVersion: () => ipcRenderer.invoke("get-app-version"),
+  selectFolder: () => ipcRenderer.invoke("select-folder"),
   isElectron: true,
 });
